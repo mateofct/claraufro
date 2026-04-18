fe9e0fbef8590bc5d05219ee13f51ec0efc58f42
POO <y.huang01@ufromail.cl>
2026-04-18 00:48:52 -0400

    Datos privados y Constructor de Usuario

---
+++ src/modelo/Usuario.java
@@ -0,0 +1,20 @@
+package modelo;
+import java.util.UUID;
+public class Usuario {
+    private String idUsuario;
+    private String idAgrupacion;
+    private String nombre;
+    private String contraseña;
+    private String rol;
+
+    public Usuario(String idAgrupacion, String nombre, String contraseña, String rol) {
+        this.idUsuario = UUID.randomUUID().toString();
+        this.idAgrupacion = idAgrupacion;
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+        this.rol = rol;
+
+
+    }
+
+}
