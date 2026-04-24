75edd67f0bdc53346828ed786baf280d7d282753
mateofct <fct.matteo@gmail.com>
2026-04-24 09:15:57 -0400

    añadido desplazamiento para cifrado

--- src/Controlador/GestorSeguridad.java
+++ src/Controlador/GestorSeguridad.java
@@ -1,4 +1,14 @@
 package Controlador;
 
 public class GestorSeguridad {
+    private static final int DESPLAZAMIENTO = 6;
+
+    public static String cifrar(String texto) {
+        if (texto == null) return null;
+        StringBuilder resultado = new StringBuilder();
+        for (char c : texto.toCharArray()) {
+            resultado.append((char) (c + DESPLAZAMIENTO));
+        }
+        return resultado.toString();
+    }
 }
