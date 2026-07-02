ebbe6ff5c9619a9f37fe85e220573b55515fc005
POO <y.huang01@ufromail.cl>
2026-07-02 00:46:51 -0400

    se modifico el launcher y ControladorUsuario, ahora cada vez que se llame a fuenteMatricula del tipo interfaz, se va a crear un objeto de la clase GestorEstudiantesUniversidad

--- src/main/java/App/Launcher.java
+++ src/main/java/App/Launcher.java
@@ -1,15 +1,11 @@
 package App;
 
-import Controlador.ControladorUsuario;
-import Controlador.ControladorFinanzas;
-import Controlador.ControladorAgrupacion;
-import Controlador.GestorMatriculas;
-import Controlador.IFuenteMatriculas;
+import Controlador.*;
 import Vista.VentanaIniciarSesion;
 
 public class Launcher {
     public static void main(String[] args) {
-        IFuenteMatriculas fuenteMatriculas = new GestorMatriculas();
+        IFuenteMatriculas fuenteMatriculas = new GestorEstudiantesUniversidad();
         ControladorUsuario controladorUsuario   = new ControladorUsuario(fuenteMatriculas); // Para poder cambiarlo si se puede, a google sheets
         ControladorFinanzas controladorFinanzas = new ControladorFinanzas();
         ControladorAgrupacion controladorAgrupacion = new ControladorAgrupacion(controladorUsuario);
