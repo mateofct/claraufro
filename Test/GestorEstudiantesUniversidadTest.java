de4a1be07d54d32fe9d10aa2273774ddc19c8816
POO <y.huang01@ufromail.cl>
2026-07-03 16:26:11 -0400

    se le agregó un nuevo constructor al GestorEstudiantesUniversidad para despues poder probar con pruebas si es que efectivamente lanza una excepcion cuando se elimina la base de datos.

--- Test/GestorEstudiantesUniversidadTest.java
+++ Test/GestorEstudiantesUniversidadTest.java
@@ -11,10 +11,9 @@ public class GestorEstudiantesUniversidadTest {
     }
 
     @Test
-    public void testBuscarNombrePorMatriculaInvalida() {
+    public void testBuscarNombrePorMatriculaNoExiste() {
         GestorEstudiantesUniversidad gestor = new GestorEstudiantesUniversidad();
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            gestor.buscarNombrePorMatricula("12345678k24");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {gestor.buscarNombrePorMatricula("12345678k24");
         });
         assertEquals("La matrícula 12345678k24 no existe en el registro base de la universidad", exception.getMessage());
     }
@@ -22,10 +21,18 @@ public class GestorEstudiantesUniversidadTest {
     @Test
     public void testBuscarNombrePorMatriculaFormatoInvalido() {
         GestorEstudiantesUniversidad gestor = new GestorEstudiantesUniversidad();
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            gestor.buscarNombrePorMatricula("12345");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {gestor.buscarNombrePorMatricula("12345");
         });
         assertTrue(exception.getMessage().contains("La matrícula no tiene un formato válido: 12345. Debe tener exactamente 11 números (incluyendo la letra 'k')"));
+    }
+
+    @Test
+    public void testBaseDatosNoEncontrada(){
+        String rutaFalsa = "data/base_que_no_existe.csv";
+        GestorEstudiantesUniversidad gestor = new GestorEstudiantesUniversidad();
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {gestor.buscarNombrePorMatricula("44444444444");
+        });
+        assertEquals("No se encontró el archivo de la base de datos de estudiantes en: " + rutaFalsa, exception.getMessage());
     }
 
 
