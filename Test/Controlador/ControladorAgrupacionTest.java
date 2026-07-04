991dd834765cec0dbf0484da3376ec5f47eb7ff2
POO <y.huang01@ufromail.cl>
2026-07-03 21:06:37 -0400

    se solucionó la duplicacion de base de datos, manteniendo las responsabilidades definidas anteriormente para cada contribuidor.

---
+++ Test/Controlador/ControladorAgrupacionTest.java
@@ -0,0 +1,14 @@
+package Controlador;
+
+import Modelo.RolUsuario;
+import Modelo.Usuario;
+import Modelo.Agrupacion;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ControladorAgrupacionTest {
+
+
+
+
+}
